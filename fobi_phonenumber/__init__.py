3edc1b7fd266b950ca1b6593fd921fa44c53786d
Artur Barseghyan <artur.barseghyan@gmail.com>
2017-05-30 21:58:40 +0200

    fix typo

--- fobi_phonenumber/__init__.py
+++ fobi_phonenumber/__init__.py
@@ -1,4 +1,4 @@
-__title__ = 'fobi_phonenumber'
+__title__ = 'fobi-phonenumber'
 __author__ = 'James Fenwick <jfenwick@tecaz.com'
 __copyright__ = '2016-2017 Tecaz Ltd'
 __license__ = 'GPL 2.0/LGPL 2.1'
@@ -9,4 +9,4 @@ __all__ = (
 
 default_app_config = 'fobi_phonenumber.apps.Config'
 
-UID = 'fomi_phonenumber'
+UID = 'fobi_phonenumber'
